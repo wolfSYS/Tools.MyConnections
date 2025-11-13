77adcd91e26ce29ee7af68632e6e1c447b908aa0
wolfSYS <develop@wolfsys.net>
2025-11-13 18:54:35 +0100

    font weight bold for public IP adr

---
+++ MyConnections/Converters/PublicIpToFontWeightBoldConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using ConnectionMgr.ExtensionMethods;
+
+namespace ConnectionMgr.Converters
+{
+	public class PublicIpToFontWeightBoldConverter : IValueConverter
+	{
+		// font weight bold for public IP adr
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is IPAddress actualIpAdr)
+			{
+				if (actualIpAdr.IsPublic())
+					return FontWeights.Bold;
+			}
+			return FontWeights.Normal;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+			=> throw new NotImplementedException();
+	}
+}
+
+
+
