8d3cc56dec53aec7dee3c947c0262907c8a8fa2a
wolfSYS <develop@wolfsys.net>
2025-11-14 13:35:53 +0100

    comment

--- MyConnections/Converters/EmptyCountToVisibilityConverter.cs
+++ MyConnections/Converters/EmptyCountToVisibilityConverter.cs
@@ -3,6 +3,12 @@ using System.Windows.Data;
 
 namespace ConnectionMgr.Converters
 {
+	/// <summary>
+	/// Converts an integer count to a <see cref="Visibility"/> value, where a count of zero results in <see
+	/// cref="Visibility.Visible"/> and any other value results in <see cref="Visibility.Collapsed"/>.
+	/// </summary>
+	/// <remarks>This converter is typically used in data binding scenarios to toggle the visibility of UI elements
+	/// based on whether a count is empty (zero) or non-empty.</remarks>
 	public class EmptyCountToVisibilityConverter : IValueConverter
 	{
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
