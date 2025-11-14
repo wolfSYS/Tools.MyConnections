22a9400cc0dea61e7274d9445cb0315b3464e14a
wolfSYS <develop@wolfsys.net>
2025-11-14 14:10:07 +0100

    bugfix empty grid / empty message

---
+++ MyConnections/Converters/EmptyCountToCollapsedConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace ConnectionMgr.Converters
+{
+	/// <summary>
+	/// Converter for hiding (Visibility.Collapsed) an empty DataGrid.
+	/// </summary>
+	public class EmptyCountToCollapsedConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is int count)
+				return count == 0 ? Visibility.Collapsed : Visibility.Visible;
+			return Visibility.Collapsed;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+			=> throw new NotImplementedException();
+	}
+}
