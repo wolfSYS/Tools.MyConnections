4102810f7169cbb07cdbe67e2b0df4fa5b23f155
wolfSYS <develop@wolfsys.net>
2025-11-14 13:30:29 +0100

    msg for empty datgrid

---
+++ MyConnections/Converters/EmptyCountToVisibilityConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace ConnectionMgr.Converters
+{
+	public class EmptyCountToVisibilityConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is int count)
+				return count == 0 ? Visibility.Visible : Visibility.Collapsed;
+			return Visibility.Collapsed;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+			=> throw new NotImplementedException();
+	}
+}
